b6f8d49adef6fb6e5ac759289a1cef4900fc5457
Shogo Goto <gotoadmn0605@gmail.com>
2025-04-22 19:42:01 +0900

    urlの検索文字列とフォーム値の同期

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Form, Link, NavLink, Outlet, useNavigation } from "react-router";
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
@@ -6,11 +7,18 @@ export async function loader({ request }: Route.LoaderArgs) {
 	const url = new URL(request.url);
 	const q = url.searchParams.get("q");
 	const contacts = await getContacts(q);
-	return { contacts };
+	return { contacts, q };
 }
 export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
-	const { contacts } = loaderData;
+	const { contacts, q } = loaderData;
 	const naviigation = useNavigation();
+
+	useEffect(() => {
+		const searchField = document.getElementById("q");
+		if (searchField instanceof HTMLInputElement) {
+			searchField.value = q || "";
+		}
+	}, [q]);
 	return (
 		<>
 			<div id="sidebar">
@@ -21,6 +29,7 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
 					<Form id="search-form" role="search">
 						<input
 							aria-label="Search contacts"
+							defaultValue={q || ""}
 							id="q"
 							name="q"
 							placeholder="Search"
