610e82408d3378c03a099ea5606af11338adf014
Shogo Goto <gotoadmn0605@gmail.com>
2025-04-22 13:43:35 +0900

    サーバーデータを型付けして呼ぶ

--- app/root.tsx
+++ app/root.tsx
@@ -9,8 +9,15 @@ import {
 import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
+import { getContacts } from "./data";
 
-export default function App() {
+export async function clientLoader() {
+	const contacts = await getContacts();
+	return { contacts };
+}
+
+export default function App({ loaderData }: Route.ComponentProps) {
+	const { contacts } = loaderData;
 	return (
 		<>
 			<div id="sidebar">
@@ -30,15 +37,30 @@ export default function App() {
 						<button type="submit">New</button>
 					</Form>
 				</div>
+
 				<nav>
-					<ul>
-						<li>
-							<Link to={`/contacts/1`}>Your Name</Link>
-						</li>
-						<li>
-							<Link to={`/contacts/2`}>Your Friend</Link>
-						</li>
-					</ul>
+					{contacts.length ? (
+						<ul>
+							{contacts.map((contact) => (
+								<li key={contact.id}>
+									<Link to={`contacts/${contact.id}`}>
+										{contact.first || contact.last ? (
+											<>
+												{contact.first} {contact.last}
+											</>
+										) : (
+											<i>名前なし</i>
+										)}
+										{contact.favorite ? <span>★</span> : null}
+									</Link>
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>
+							<i>コンタクトなし</i>
+						</p>
+					)}
 				</nav>
 			</div>
 			<div id="detail">
