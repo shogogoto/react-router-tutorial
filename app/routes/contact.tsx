29d6e47c6f285d9dde96a52f38ff102ddde1581d
Shogo Goto <gotoadmn0605@gmail.com>
2025-04-22 16:20:20 +0900

    contactごとに対応

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,17 +1,17 @@
 import { Form } from "react-router";
+import { getContact } from "../data";
+import type { Route } from "./+types/contact";
 
-import type { ContactRecord } from "../data";
+export async function loader({ params }: Route.LoaderArgs) {
+	const contact = await getContact(params.contactId);
+	if (!contact) {
+		throw new Response("Not Found", { status: 404 });
+	}
+	return { contact };
+}
 
-export default function Contact() {
-	const contact = {
-		first: "Your",
-		last: "Name",
-		avatar: "https://placecats.com/200/200",
-		twitter: "your_handle",
-		notes: "Some notes",
-		favorite: true,
-	};
-
+export default function Contact({ loaderData }: Route.ComponentProps) {
+	const { contact } = loaderData;
 	return (
 		<div id="contact">
 			<div>
